5886078dfde48c970c5e4dcb7207ce0459a3362a
mchuguinho <hugodinis2001@gmail.com>
2024-06-01 21:06:14 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'pt.ihn.roteiro',
   appName: 'roteiros',
   webDir: 'www'
 };
